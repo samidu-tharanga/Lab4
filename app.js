74dcc436d6236ca12768efd621e51bf12dfc9d95
samidu-tharanga <stharanga.socialcontact@gmail.com>
2026-02-24 00:38:45 +0530

    removefs

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ console.log(data);
 }); 
 
 //write file
-const fs = require('fs'); 
 fs.writeFile('file.txt', 'Hello World!', function (err) { 
   if (err) throw err; 
   console.log('File saved!'); 
